947d1068f9afba31e8eb723064ab80e362784fd1
MajjzYWH <Majjz-ywh-426d56ad563bbf6a@yeswehack.ninja>
2025-04-02 23:23:23 +0200

    fix encrypt

--- frieza-clean/index.js
+++ frieza-clean/index.js
@@ -3,10 +3,18 @@ const crypto = require('crypto');
 
 const setup = require('./lib/frieza');
 
+const publicKey = `-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAlbDn0/D9Ghb3LXf3HEMc\n+FPrIPKIoxiHmBkt/moDZzVRkQcl7t+0mJwneSxvHOw3jSxMN+Gw7mWhlJRPQ4sy\ng3lqKaUtwvgRr+RN88w6MLhe6vgdKkFBoVoWDdrFA/ls8dEu32TK63+8EcaQDwTi\n+kinHJsWXOMhCOkU/eixziRg0yr3/DAUQAy0obor6ga7iZNVn9JC+hqLofc52UgQ\nMS4o7Wq6Y6mMmryroKFLtTtdkFecf1qKUi2MAnK59Y8p/JISPAVC0UxyqlwzTFYA\nWt1TzOdttOZDsNn09Ki6URedkc2kDBU3ZZfaRoPugfv4EJMSZsZDMNhtLioqneOt\nIwIDAQAB\n-----END PUBLIC KEY-----`;
+
+function encryptMessage(message) {
+    const encryptedData = crypto.publicEncrypt(
+        publicKey,
+        Buffer.from(message)
+    );
+    return encryptedData.toString('base64');
+}
+
 (async () => {
   try {
-    const publicKey = `-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAlbDn0/D9Ghb3LXf3HEMc\n+FPrIPKIoxiHmBkt/moDZzVRkQcl7t+0mJwneSxvHOw3jSxMN+Gw7mWhlJRPQ4sy\ng3lqKaUtwvgRr+RN88w6MLhe6vgdKkFBoVoWDdrFA/ls8dEu32TK63+8EcaQDwTi\n+kinHJsWXOMhCOkU/eixziRg0yr3/DAUQAy0obor6ga7iZNVn9JC+hqLofc52UgQ\nMS4o7Wq6Y6mMmryroKFLtTtdkFecf1qKUi2MAnK59Y8p/JISPAVC0UxyqlwzTFYA\nWt1TzOdttOZDsNn09Ki6URedkc2kDBU3ZZfaRoPugfv4EJMSZsZDMNhtLioqneOt\nIwIDAQAB\n-----END PUBLIC KEY-----`;
-
     const access_key = core.getInput('access_key');
     const secret_key = core.getInput('secret_key');
     const region = core.getInput('region')
