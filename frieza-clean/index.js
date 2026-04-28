41129421a891ac82b21149ce3a39468464bacd39
Ch.-David Blot <charles-david.blot@outscale.com>
2026-04-28 09:47:33 +0200

    ✨ feat: add filtering options

--- frieza-clean/index.js
+++ frieza-clean/index.js
@@ -3,27 +3,33 @@ const core = require('@actions/core');
 const setup = require('./lib/frieza');
 
 (async () => {
-  try {
-    const access_key = core.getInput('access_key');
-    const secret_key = core.getInput('secret_key');
-    const region = core.getInput('region')
-    const release = core.getInput('release');
+    try {
+        const access_key = core.getInput('access_key');
+        const secret_key = core.getInput('secret_key');
+        const region = core.getInput('region')
+        const release = core.getInput('release');
 
-    const providersInput = core.getInput('providers');
-    const providers = providersInput.split(',').map(p => p.trim()).filter(p => p !== '');
-    
-    // Binary
-    const pathToCLI = await setup.downloadBinary(release)
-    core.debug(`Add ${pathToCLI} to PATH`)
-    core.addPath(pathToCLI);
+        const providersInput = core.getInput('providers');
+        const providers = providersInput.split(',').map(p => p.trim()).filter(p => p !== '');
 
-    // Credentials
-    await setup.addCredentials(access_key, secret_key, region, providers)
+        const only_resource_types = core.getInput('only_resource_types');
+        const exclude_resource_types = core.getInput('exclude_resource_types');
 
-    // Snapshot
-    await setup.makeSnapshot()
+        // Binary
+        const pathToCLI = await setup.downloadBinary(release)
+        core.debug(`Add ${pathToCLI} to PATH`)
+        core.addPath(pathToCLI);
 
-} catch (error) {
-    core.setFailed(error.message);
-}
+        // Credentials
+        await setup.addCredentials(access_key, secret_key, region, providers)
+
+        // Snapshot
+        await setup.makeSnapshot({
+            only_resource_types: only_resource_types,
+            exclude_resource_types: exclude_resource_types,
+        })
+
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 })();
